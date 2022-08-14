1ab918cb534bea60e5f5d57362572e8ffe60d638
stmhyd <ydhwang@irexenc.com>
2022-08-14 12:33:18 +0900

    initial commit

---
+++ src/main/kotlin/com/agiledeveloper/todo/TaskController.kt
@@ -0,0 +1,39 @@
+package com.agiledeveloper.todo
+
+import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+
+@RestController
+@RequestMapping("/task")
+/*
+class TaskController {
+
+  @GetMapping
+  fun tasks() = "to be implemented"
+  }
+*/
+class TaskController(val service: TaskService) {
+
+  @GetMapping("/hello")
+  fun hello() = "hello hyd"
+
+  @GetMapping
+  fun tasks() = ResponseEntity.ok(service.getAll())
+//  fun tasks() = "hello hyd"
+
+  @PostMapping
+  fun create(@RequestBody task: Task): ResponseEntity<String> {
+    val result = service.save(task)
+
+    return ResponseEntity.ok(
+      "added task with description ${result.description}")
+  }
+
+  @DeleteMapping("/{id}")
+  fun delete(@PathVariable id: Long) = if (service.delete(id)) {
+    ResponseEntity.ok("Task with id $id deleted")
+  } else {
+    ResponseEntity.status(404).body("Task with id $id not found")
+  }
+  
+}          
